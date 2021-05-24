9169a2f085961e0c98114bfaa4e81d788adc8eda
Aswin A V <aswinavofficial@gmail.com>
2021-05-24 21:48:03 +0530

    DelegatingPasswordEncoder
    Bcrypt - default password encoder

--- src/main/java/com/letslearn/eventify/config/WebSecurityConfig.java
+++ src/main/java/com/letslearn/eventify/config/WebSecurityConfig.java
@@ -1,5 +1,8 @@
 package com.letslearn.eventify.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,8 +10,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 
 
 @EnableWebSecurity
@@ -19,7 +27,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 		@Bean
 		public PasswordEncoder passwordEncoder() {
 			
-			return NoOpPasswordEncoder.getInstance();
+			String idForEncode = "bcrypt";
+			/**
+			 * this would be used as the default encoding method when
+			 * calling passwordEncoder().encode("password") method
+			 */			
+			Map<String,PasswordEncoder> idToPasswordEncoder = new HashMap<>();
+			idToPasswordEncoder.put(idForEncode, new BCryptPasswordEncoder());
+			idToPasswordEncoder.put("noop", NoOpPasswordEncoder.getInstance());
+			idToPasswordEncoder.put("pbkdf2", new Pbkdf2PasswordEncoder());
+			idToPasswordEncoder.put("scrypt", new SCryptPasswordEncoder());
+			idToPasswordEncoder.put("sha256", new StandardPasswordEncoder());
+			
+			
+			return new DelegatingPasswordEncoder(idForEncode, idToPasswordEncoder);
 		}
 	
 	 	@Override
@@ -41,8 +62,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	 	@Override
 	 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 	 		auth
-	 		.inMemoryAuthentication().withUser("user").password("password").roles("USER")
-	 				.and().withUser("admin").password("password").roles("USER", "ADMIN");
+	 		.inMemoryAuthentication()
+	 		.withUser("user").password("password").roles("USER")
+	 				.and().withUser("admin").password(passwordEncoder().encode("password")).roles("USER", "ADMIN");
 	 	}
 
 
